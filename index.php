2c58c8be29ce7a867fe4390f580c5677228ec269
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-12-17 16:17:29 +0100

    Starting

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="dist/css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="general-container d-flex flex-clmn">
+    <div class="header">
+        <img class="logo-header" src="logo.png" alt="">
+    </div>
+    <div class="main">
+        <div class="container-main">
+            <div class="container-album d-flex">
+                <div class="single-album">
+
+                </div>
+                <div class="single-album">
+
+                </div>
+                <div class="single-album">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
