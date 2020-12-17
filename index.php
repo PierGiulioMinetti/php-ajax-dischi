ddeb207e466a721305fe3e196b50b28581c81b8b
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-12-18 00:59:12 +0100

    Fixed

--- index.php
+++ index.php
@@ -1,60 +1,61 @@
-<?php 
-    include __DIR__ .'/database.php';
+<?php
+include __DIR__ . '/database.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<link rel="stylesheet" href="dist/css/style.css">
+    <link rel="stylesheet" href="dist/css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <!-- GENERAL CONTAINER -->
     <div class="general-container d-flex flex-clmn">
         <!-- HEADER -->
-    <div class="header">
-        <img class="logo-header" src="logo.png" alt="">
-    </div>
-    <!-- MAIN -->
-    <div class="main">
-        <div class="container-main">
-            <div class="container-album d-flex">
+        <div class="header">
+            <img class="logo-header" src="logo.png" alt="">
+        </div>
+        <!-- MAIN -->
+        <div class="main">
+            <div class="container-main">
+                <div class="container-album d-flex">
+                    <?php foreach ($database as $value) { ?>
+                        <div class="single-album ">
 
-                <div class="single-album ">
-                    <?php foreach($database as $value) { ?>
-                    <div class="album-cover d-flex">
-                        <img src="<?php echo $value['poster'] ?>" alt="">
-                    </div>
-                    <div class="album-description">
-                        <p> 
-                            <?php echo $value['title'] ?>     
-                        </p>
-                        <p>
-                        <?php echo $value['author'] ?> 
-                        </p>
-                        <p>
-                        <?php echo $value['year'] ?> 
-                        </p>
-                        
-                        <p>
-                        <?php echo $value['genre'] ?> 
-                        </p>
-            
+                            <div class="album-cover d-flex">
+                                <img src="<?php echo $value['poster'] ?>" alt="">
+                            </div>
+                            <div class="album-description">
+                                <p>
+                                    <?php echo $value['title'] ?>
+                                </p>
+                                <p>
+                                    <?php echo $value['author'] ?>
+                                </p>
+                                <p>
+                                    <?php echo $value['year'] ?>
+                                </p>
 
-                        <!-- PHP -->
-                        <?php }
-                
-                        ?>
+                                <p>
+                                    <?php echo $value['genre'] ?>
+                                </p>
 
-                        <!-- PHP -->
-                    </div>
+
+                                <!-- PHP -->
+                            
+
+                            <!-- PHP -->
+                            </div>
+                        </div>
+                    <?php } ?>    
                 </div>
-                
             </div>
         </div>
     </div>
-    </div>
 </body>
+
 </html>
